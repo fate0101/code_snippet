87ce7b0cf213b2adfad4e6ee71b4a5359cc3aea8
faTe <44430265+fate0101@users.noreply.github.com>
2019-01-15 18:10:30 +0800

    Add files via upload

---
+++ fcpool/nt_driver_test/config.h
@@ -0,0 +1,44 @@
+﻿#ifndef config_h
+#define config_h
+
+/*
+  初始化 likely 和 unlikeyly
+*/
+#ifndef GCC
+#define likely
+#define unlikely
+#endif
+
+/*
+  是否允许断言
+*/
+//#ifndef FC_ASSERT
+//#define FC_ASSERT
+//#include <assert.h>
+//#endif
+
+/*
+  是否拥有默认初始化方案
+*/
+//#ifndef FC_MEM_DEFAULT
+//#define FC_MEM_DEFAULT
+//#include <stdlib.h>
+//#endif
+
+/*
+  是否打印调试日志
+*/
+// #define FC_MEM_DBG
+
+/*
+  来自 fcerr
+*/
+enum ERROR_CODE {
+  F_OK = 0,
+  F_FAILED = -1,
+  F_UNSUPPORT = -2,
+};
+
+#define FC_SUCCESS(x) (x >= F_OK)
+
+#endif  // config_h
---
+++ fcpool/nt_driver_test/main.c
@@ -0,0 +1,62 @@
+﻿#include <ntddk.h>
+
+#include "fcalloctor.h"
+
+void unload(PDRIVER_OBJECT obj) {
+	obj;
+}
+
+void* _cdecl alloc_s(size_t s){
+	return ExAllocatePoolWithTag(PagedPool, s, 'fcp');
+}
+
+void _cdecl free_s(void* p) {
+	ExFreePoolWithTag(p, 'fcp');
+}
+
+void* _cdecl realloc_s(void* p, size_t ns) {
+	size_t os = GET_SIZE_BY_POINTER(p);
+
+	void* np = ExAllocatePoolWithTag(PagedPool, ns, 'fcp');
+	if (NULL == np)
+		return np;
+
+	// 选短的那个
+	RtlCopyMemory(np, p, ns <= os ? ns : os);
+
+	ExFreePoolWithTag(p, 'fcp');
+	return np;
+}
+
+void BSOD() {
+	// do nothing
+	return;
+}
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT obj, PUNICODE_STRING reg_path) {
+	reg_path;
+
+	obj->DriverUnload = unload;
+
+	M_ENV m_env = { sizeof(m_env) };
+
+	m_env.ins.malloc_fn  = alloc_s;
+	m_env.ins.free_fn    = free_s;
+	m_env.ins.realloc_fn = realloc_s;
+	m_env.ins.BSOD       = BSOD;
+
+	if (!FC_SUCCESS(FInitAllocator(m_env))) {
+		return STATUS_UNSUCCESSFUL;
+	}
+
+	for (size_t i = 0; i < 10000; i++) {
+
+		char* t = m_env.FAllocate(m_env, 100);
+
+		m_env.FDeallocate(m_env, t);
+	}
+
+	FDestoryAllocator(m_env);
+
+	return STATUS_SUCCESS;
+}
